03398bd27c1613e79018e70dbaba2362f1220f8d
S.P. Mohanty <spmohanty91@gmail.com>
2018-07-22 06:17:43 +0200

    Working implementation of evaluation loop

--- renderer.py
+++ renderer.py
@@ -4,6 +4,8 @@ import os
 from jinja2 import Environment, FileSystemLoader
 import uuid
 import json
+import yaml
+import time
 
 class CrowdAIRenderer:
     def __init__(self, challenge_config, templates_folder=False):
@@ -18,24 +20,48 @@ class CrowdAIRenderer:
         self.file_loader = FileSystemLoader(self.templates_folder)
         self.jinja_env = Environment(loader=self.file_loader)
 
+        # Render Cache settings
+        self.last_note_text = ""
+        self.global_issue_update_frequency = 5
+        self.last_issue_note_update_time = time.time()
+
     def render_pod_spec(self, params):
         template = self.jinja_env.get_template("pod_spec/pod_spec.yaml")
-        return template.render(
+        pod_spec = yaml.load(template.render(
             challenge_config=self.challenge_config,
             params=params
-            )
+            ))
+        return pod_spec
+
     def render_evaluation_state(self, evaluation_state):
         _evaluation_state = {
             "state" : "execution_pending",
             "steps" : [],
         }
         _evaluation_state.update(evaluation_state)
-        template = self.jinja_env.get_template(
-            "evaluation_state/evaluation_state.yaml"
-            )
-        return template.render(
-            evaluation_state=_evaluation_state
-        )
+        return """
+```
+
+        {}
+
+```
+        """.format(json.dumps(evaluation_state, indent=4))
+        # template = self.jinja_env.get_template(
+        #     "evaluation_state/evaluation_state.yaml"
+        #     )
+        # return template.render(
+        #     evaluation_state=_evaluation_state
+        # )
+
+    def update_evaluation_log(self, evaluation_state, gl_note, force_update=False):
+        if force_update or (time.time() - self.last_issue_note_update_time > self.global_issue_update_frequency):
+            note_text = self.render_evaluation_state(evaluation_state)
+            if note_text != self.last_note_text and note_text.strip() != "":
+                gl_note.body = note_text
+                gl_note.save()
+                self.last_issue_note_update_time = time.time()
+                self.last_note_text = note_text
+
 
 if __name__ == "__main__":
 
